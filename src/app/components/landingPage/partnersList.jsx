7d493cf6449130c3fc665743abfc26a9744e84a8
devmoatassem <moatassemsindhu@gmail.com>
2024-02-02 01:43:40 +0500

    partners list flex corrected

--- src/app/components/landingPage/partnersList.jsx
+++ src/app/components/landingPage/partnersList.jsx
@@ -25,13 +25,13 @@ const logos = [
 
 const brandlist = logos.map((logo,index)=>{ 
     return (
-       <Image key={index} src={logo.path} alt={logo.name} width={200} height={200} className='h-4 mx-auto md:h-7 my-5 lg:my-0' />
+       <Image key={index} src={logo.path} alt={logo.name} width={200} height={200} className='h-4 mx-auto md:h-5 my-5 lg:my-0' />
     )
 })
 
 const PartnersList = () => {
   return (
-    <div className='flex flex-wrap justify-between items-center py-8 px-4 shadow-lg drop-shadow rounded-lg'>
+    <div className='flex flex-wrap lg:flex-nowrap justify-between items-center py-8 px-4 shadow-lg drop-shadow rounded-lg'>
         {brandlist}
     </div>
   )
