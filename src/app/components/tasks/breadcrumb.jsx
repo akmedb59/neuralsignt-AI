502cea06ca1638f0b01e3917f2ac72043d960754
devmoatassem <moatassemsindhu@gmail.com>
2024-02-03 13:18:44 +0500

    user Tasks Page added

--- src/app/components/tasks/breadcrumb.jsx
+++ src/app/components/tasks/breadcrumb.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import Button from "../common/button";
 import { LuListFilter } from "react-icons/lu";
-const Breadcrumb = ({ setmodalIsOpen }) => {
-  
-
+const Breadcrumb = ({ setmodalIsOpen, isAdmin }) => {
   return (
     <div className="border-b py-10">
-     
       <div className="flex items-center justify-between">
         <div className="flex items-baseline gap-2">
           <h1 className="text-2xl font-bold">Tasks</h1>
           <span className="text-sm text-gray-600">Showing All</span>
         </div>
-        <div className="flex items-center gap-4">
-          <div onClick={() => setmodalIsOpen(true)}>
-            <Button
-              button={"+ Add Task"}
-              customClass="bg-btgreen text-white text-sm rounded-lg w-fit px-5"
-            />
-          </div>
 
+        <div className="flex items-center gap-4">
+          {isAdmin ? (
+            <div onClick={() => setmodalIsOpen(true)}>
+              <Button
+                button={"+ Add Task"}
+                customClass="bg-btgreen text-white text-sm rounded-lg w-fit px-5"
+              />
+            </div>
+          ) : null}
           <div className="flex items-center gap-2">
             <div className="aspect-square rounded-full bg-white p-2">
               <LuListFilter />
--- src/app/user/tasks/page.jsx
+++ src/app/user/tasks/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import TaskCard from "@/app/components/tasks/taskCard";
+import Breadcrumb from "@/app/components/tasks/breadcrumb";
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div className="h-full min-h-screen w-full bg-bggrey">
+      <div className="mx-auto w-full px-10 py-5 lg:px-36 2xl:max-w-[1400px]">
+        <Breadcrumb isAdmin={false} />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10">
+          {Array.from({ length: 11 }, (_, index) => (
+            <TaskCard key={index} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
